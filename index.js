c279f43dde89f35bf1c31adeef59f03ee498efef
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-26 12:30:47 -0600

    chore: Add error handling middleware and update error handling in Express server

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const { config } = require('./src/config/config');
 
 const express = require('express');
 const configureCors = require('./src/middlewares/cors');
+const {
+	logErrors,
+	ormErrorHandler,
+	boomErrorHandler,
+	errorHandler,
+} = require('./src/middlewares/error.handler');
 const PORT = config.port;
 
 const app = express();
@@ -16,7 +22,10 @@ app.get('/', (req, res) => {
 
 //Routes
 
-//Errors Middleware
+app.use(logErrors);
+app.use(ormErrorHandler);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
