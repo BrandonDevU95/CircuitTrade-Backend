9e1d98de376c7d711b17b357e7f4f9b65494a66f
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-26 12:28:03 -0600

    feat: Add authentication middleware

---
+++ src/middlewares/auth.handler.js
@@ -0,0 +1,32 @@
+const boom = require('@hapi/boom');
+const { config } = require('dotenv');
+
+function checkApiKey(req, res, next) {
+	const apiKey = req.headers['api'];
+	if (apiKey === config.apiKey) {
+		next();
+	} else {
+		next(boom.unauthorized('Invalid API Key'));
+	}
+}
+
+function checkAdminRole(req, res, next) {
+	const user = req.user;
+	if (user.role === 'admin') {
+		next();
+	} else {
+		next(boom.unauthorized('Invalid Role'));
+	}
+}
+
+function checkRoles(...roles) {
+	return (req, res, next) => {
+		if (!roles.includes(req.user.role)) {
+			next(boom.unauthorized('Invalid Role'));
+		} else {
+			next();
+		}
+	};
+}
+
+module.exports = { checkApiKey, checkAdminRole, checkRoles };
