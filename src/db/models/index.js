6defe4f1f6a363d60cb203cbaa687468a0a54608
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2025-02-07 20:35:04 -0600

    feat: Initialize Company model in Sequelize setup

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -5,10 +5,12 @@ const { OrderDetail, OrderDetailSchema } = require('./order-detail.model');
 const { Company, CompanySchema } = require('./company.model');
 
 function setupModels(sequelize) {
+	Company.init(CompanySchema, Company.config(sequelize));
 	// User.init(UserSchema, User.config(sequelize));
 	// Product.init(ProductSchema, Product.config(sequelize));
 	// Order.init(OrderSchema, Order.config(sequelize));
 	// OrderDetail.init(OrderDetailSchema, OrderDetail.config(sequelize));
+
 	// Asociaciones
 	// User.associate(sequelize.models);
 	// Product.associate(sequelize.models);
