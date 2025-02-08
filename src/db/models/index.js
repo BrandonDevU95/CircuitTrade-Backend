2a0e9a0896b203669c96082b746b8e6e8a7aafd1
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2025-02-07 18:31:26 -0600

    feat: Add Company model initialization and associations in setupModels function

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -2,18 +2,18 @@ const { User, UserSchema } = require('./user.model');
 const { Product, ProductSchema } = require('./product.model');
 const { Order, OrderSchema } = require('./order.model');
 const { OrderDetail, OrderDetailSchema } = require('./order-detail.model');
+const { Company, CompanySchema } = require('./company.model');
 
 function setupModels(sequelize) {
-	User.init(UserSchema, User.config(sequelize));
-	Product.init(ProductSchema, Product.config(sequelize));
-	Order.init(OrderSchema, Order.config(sequelize));
-	OrderDetail.init(OrderDetailSchema, OrderDetail.config(sequelize));
-
+	// User.init(UserSchema, User.config(sequelize));
+	// Product.init(ProductSchema, Product.config(sequelize));
+	// Order.init(OrderSchema, Order.config(sequelize));
+	// OrderDetail.init(OrderDetailSchema, OrderDetail.config(sequelize));
 	// Asociaciones
-	User.associate(sequelize.models);
-	Product.associate(sequelize.models);
-	Order.associate(sequelize.models);
-	OrderDetail.associate(sequelize.models);
+	// User.associate(sequelize.models);
+	// Product.associate(sequelize.models);
+	// Order.associate(sequelize.models);
+	// OrderDetail.associate(sequelize.models);
 }
 
 module.exports = setupModels;
