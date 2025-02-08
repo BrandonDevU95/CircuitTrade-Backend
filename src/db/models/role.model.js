c217e8e5fa9fee65a240fb4e828b8e2f9f234046
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2025-02-08 11:53:22 -0600

    feat: Add Role model with associations to User model

---
+++ src/db/models/role.model.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+
+const ROLE_TABLE = 'roles';
+
+const RoleSchema = {
+	id: {
+		allowNull: false,
+		autoIncrement: true,
+		primaryKey: true,
+		type: DataTypes.INTEGER,
+	},
+	name: {
+		allowNull: false,
+		type: DataTypes.STRING,
+	},
+	description: {
+		allowNull: false,
+		type: DataTypes.STRING,
+	},
+};
+
+class Role extends Model {
+	static associate(models) {
+		this.belongsToMany(models.User, {
+			through: 'user_roles',
+			foreignKey: 'roleId',
+			otherKey: 'userId',
+			as: 'users',
+		});
+	}
+
+	static config(sequelize) {
+		return {
+			sequelize,
+			tableName: ROLE_TABLE,
+			modelName: 'Role',
+			timestamps: true,
+		};
+	}
+}
+
+module.exports = { Role, RoleSchema, ROLE_TABLE };
