d0f9f7eb4b219ae978eaf64752537ea901276b0c
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2025-02-07 18:24:21 -0600

    feat: Add Company model with schema and associations

---
+++ src/db/models/company.model.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require('sequelize');
+
+const COMPANY_TABLE = 'companies';
+
+const CompanySchema = {
+	id: {
+		allowNull: false,
+		autoIncrement: true,
+		primaryKey: true,
+		type: DataTypes.INTEGER,
+	},
+	name: {
+		allowNull: false,
+		type: DataTypes.STRING,
+	},
+	rfc: {
+		allowNull: false,
+		type: DataTypes.STRING,
+		maxLength: 13,
+		unique: true,
+	},
+	address: {
+		allowNull: false,
+		type: DataTypes.STRING,
+	},
+	phone: {
+		allowNull: false,
+		type: DataTypes.STRING,
+	},
+	email: {
+		allowNull: false,
+		type: DataTypes.STRING,
+	},
+	type: {
+		allowNull: false,
+		type: DataTypes.ENUM('supplier', 'buyer', 'hybrid'),
+	},
+};
+
+class Company extends Model {
+	static associate(models) {
+		// this.hasMany(models.Product, {
+		//     as: 'products',
+		//     foreignKey: 'companyId',
+		// });
+	}
+
+	static config(sequelize) {
+		return {
+			sequelize,
+			tableName: COMPANY_TABLE,
+			modelName: 'Company',
+			timestamps: true,
+		};
+	}
+}
+
+module.exports = { Company, CompanySchema, COMPANY_TABLE };
