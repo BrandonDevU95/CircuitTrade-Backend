80a7fa4a8342c52ffdd1b3912e8da7b86732effb
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2025-02-12 17:55:50 -0600

    feat: Implement UserService for user management with create, update, delete, and retrieval methods

---
+++ src/services/user.service.js
@@ -0,0 +1,78 @@
+const boom = require('@hapi/boom');
+
+const { models } = require('./../lib/sequelize');
+
+class userService {
+	constructor() {
+		this.model = models.User;
+	}
+
+	async create(data) {
+		//TODO: Implementar transacciones para asegurar que se creen los usuarios y la empresa
+		const { rfc, email, ...userData } = data;
+		const emailLower = email.toLowerCase();
+
+		const existingUser = await this.model.findOne({
+			where: {
+				email: emailLower,
+			},
+		});
+
+		if (existingUser) {
+			throw boom.badRequest('user already exists');
+		}
+
+		const company = await models.Company.findOne({
+			where: {
+				rfc: rfc,
+			},
+		});
+
+		if (!company) {
+			throw boom.badRequest('company not found');
+		}
+
+		const newUser = await this.model.create({
+			...userData,
+			email: emailLower,
+			companyId: company.id,
+		});
+
+		return newUser;
+	}
+
+	async update(id, data) {
+		const user = await this.model.findByPk(id);
+		if (!user) {
+			throw boom.notFound('user not found');
+		}
+
+		const updatedUser = await user.update(data);
+		return updatedUser;
+	}
+
+	async delete(id) {
+		const user = await this.model.findByPk(id);
+		if (!user) {
+			throw boom.notFound('user not found');
+		}
+
+		await user.destroy();
+		return { id };
+	}
+
+	async find() {
+		const users = await this.model.findAll();
+		return users;
+	}
+
+	async findOne(id) {
+		const user = await this.model.findByPk(id);
+		if (!user) {
+			throw boom.notFound('user not found');
+		}
+		return user;
+	}
+}
+
+module.exports = userService;
