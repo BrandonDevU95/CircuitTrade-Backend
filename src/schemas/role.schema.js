17001024235f74f28c39cc9b47729e4e1325a820
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2025-02-12 15:14:52 -0600

    feat: Add role schema with validation for creating, updating, and retrieving roles

---
+++ src/schemas/role.schema.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+
+const id = Joi.number().integer();
+const name = Joi.string().min(3).max(100);
+const description = Joi.string().min(3).max(255);
+
+const createRoleSchema = Joi.object({
+	name: name.required(),
+	description: description.required(),
+});
+
+const updateRoleSchema = Joi.object({
+	name: name,
+	description: description,
+});
+
+const getRoleSchema = Joi.object({
+	id: id.required(),
+});
+
+module.exports = {
+	createRoleSchema,
+	updateRoleSchema,
+	getRoleSchema,
+};
