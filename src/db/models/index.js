f8e80f1781685757bc5ab352b7393d77f845b666
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2025-02-13 20:07:11 -0600

    feat: Remove UserRole model and its associations to simplify database structure

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,19 +1,16 @@
 const { Company, CompanySchema } = require('./company.model');
 const { User, UserSchema } = require('./user.model');
 const { Role, RoleSchema } = require('./role.model');
-const { UserRole, UserRoleSchema } = require('./user-role.model');
 
 function setupModels(sequelize) {
 	Company.init(CompanySchema, Company.config(sequelize));
 	User.init(UserSchema, User.config(sequelize));
 	Role.init(RoleSchema, Role.config(sequelize));
-	UserRole.init(UserRoleSchema, UserRole.config(sequelize));
 
 	// Asociaciones
 	Company.associate(sequelize.models);
 	User.associate(sequelize.models);
 	Role.associate(sequelize.models);
-	UserRole.associate(sequelize.models);
 }
 
 module.exports = setupModels;
