8302b85f09c9601c4b5383ee4a9abcd47146d22e
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2025-02-21 15:46:41 -0600

    feat: Add sign-in validation schema and middleware to authentication route

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const passport = require('passport');
 const JWTManager = require('../utils/jwt');
+const validatorHandler = require('../middlewares/validator.handler');
+const { signInSchema } = require('../schemas/auth.schema');
 
 const router = express.Router();
 
 router.post(
 	'/sign-in',
+	validatorHandler(signInSchema, 'body'),
 	passport.authenticate('local', { session: false }),
 	async (req, res, next) => {
 		try {
