ad242af8eecb45ef08200f2df387103dc3ba35d9
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2025-02-25 12:27:46 -0600

    feat: Add RefreshToken model and integrate with Sequelize setup

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,16 +1,19 @@
 const { Company, CompanySchema } = require('./company.model');
 const { User, UserSchema } = require('./user.model');
 const { Role, RoleSchema } = require('./role.model');
+const { RefreshToken, RefreshTokenSchema } = require('./refreshToken.model');
 
 function setupModels(sequelize) {
 	Company.init(CompanySchema, Company.config(sequelize));
 	User.init(UserSchema, User.config(sequelize));
 	Role.init(RoleSchema, Role.config(sequelize));
+	RefreshToken.init(RefreshTokenSchema, RefreshToken.config(sequelize));
 
 	// Asociaciones
 	Company.associate(sequelize.models);
 	User.associate(sequelize.models);
 	Role.associate(sequelize.models);
+	RefreshToken.associate(sequelize.models);
 }
 
 module.exports = setupModels;
---
+++ src/db/models/refreshToken.model.js
@@ -0,0 +1,63 @@
+const { Model, DataTypes } = require('sequelize');
+
+const REFRESH_TOKEN = 'refresh_tokens';
+
+const RefreshTokenSchema = {
+	id: {
+		allowNull: false,
+		autoIncrement: true,
+		primaryKey: true,
+		type: DataTypes.INTEGER,
+	},
+	token: {
+		allowNull: false,
+		type: DataTypes.STRING,
+		unique: true,
+	},
+	userId: {
+		allowNull: false,
+		type: DataTypes.INTEGER,
+	},
+	expiresAt: {
+		allowNull: false,
+		type: DataTypes.DATE,
+	},
+};
+
+class RefreshToken extends Model {
+	static associate(models) {
+		this.belongsTo(models.User, {
+			foreignKey: 'userId',
+			as: 'user',
+			allowNull: false,
+			onDelete: 'CASCADE',
+		});
+	}
+
+	static config(sequelize) {
+		return {
+			sequelize,
+			tableName: REFRESH_TOKEN,
+			modelName: 'RefreshToken',
+			timestamps: true,
+			hooks: {
+				beforeSave: async (refreshToken) => {
+					if (refreshToken.changed('token')) {
+						refreshToken.token = refreshToken.token.trim();
+					}
+				},
+			},
+			indexes: [
+				{
+					unique: true,
+					fields: ['token'],
+				},
+				{
+					fields: ['expiresAt'],
+				},
+			],
+		};
+	}
+}
+
+module.exports = { RefreshToken, RefreshTokenSchema, REFRESH_TOKEN };
