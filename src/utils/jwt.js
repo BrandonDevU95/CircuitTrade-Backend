35d7eab4d7a9f9f0dc13b9a4982083e186b90589
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2025-02-25 18:42:52 -0600

    feat: Refactor JWTManager to separate access and refresh token generation, add verification and expiration check methods

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,24 +2,34 @@ const jwt = require('jsonwebtoken');
 const { config } = require('../config/config');
 
 class JWTManager {
-	static generateTokens(user) {
-		const accessToken = jwt.sign(
-			{ sub: user.id, role: user.role },
-			config.jwtSecret,
-			{ expiresIn: '15m' }
-		);
+	static generateAccessToken(user) {
+		return jwt.sign({ sub: user.id, role: user.role }, config.jwtSecret, {
+			expiresIn: '1m',
+		});
+	}
 
-		const refreshToken = jwt.sign(
-			{ sub: user.id },
-			config.jwtRefreshSecret,
-			{ expiresIn: '7d' }
-		);
+	static generateRefreshToken(user) {
+		return jwt.sign({ sub: user.id }, config.jwtRefreshSecret, {
+			expiresIn: '7d',
+		});
+	}
 
-		return { accessToken, refreshToken };
+	static verifyAccessToken(token) {
+		return jwt.verify(token, config.jwtSecret);
 	}
 
 	static verifyRefreshToken(token) {
 		return jwt.verify(token, config.jwtRefreshSecret);
+	}
+
+	static decodeToken(token) {
+		return jwt.decode(token);
+	}
+
+	static isExpired(token) {
+		const { exp } = jwt.decode(token);
+		const currentTime = Date.now() / 1000;
+		return currentTime > exp;
 	}
 }
 
