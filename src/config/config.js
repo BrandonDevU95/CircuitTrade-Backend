2ef1692c09c2d87a3cd98da69efbf40015e48886
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2025-03-07 21:02:30 -0600

    feat: Restructure configuration object to improve organization and add required environment variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,24 +1,47 @@
 require('dotenv').config();
 
+const requiredEnvVars = [
+	'DB_USER',
+	'DB_PASSWORD',
+	'DB_HOST',
+	'DB_NAME',
+	'API_KEY',
+	'JWT_SECRET',
+	'JWT_REFRESH_SECRET',
+	'BCRYPT_SALT'
+];
+
 const config = {
-	env: process.env.NODE_ENV || 'development',
-	port: process.env.PORT || 3000,
-	version: process.env.VERSION,
-	dbUser: process.env.DB_USER,
-	dbPassword: process.env.DB_PASSWORD,
-	dbHost: process.env.DB_HOST,
-	dbName: process.env.DB_NAME,
-	dbPort: process.env.DB_PORT,
-	apiKey: process.env.API_KEY,
-	jwtSecret: process.env.JWT_SECRET,
-	jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
-	bcryptSaltRounds: parseInt(process.env.BCRYPT_SALT),
+	node: {
+		env: process.env.NODE_ENV || 'development',
+		port: process.env.PORT || 3000,
+		version: process.env.VERSION
+	},
+	db: {
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		host: process.env.DB_HOST,
+		name: process.env.DB_NAME,
+		port: process.env.DB_PORT || 3306
+	},
+	jwt: {
+		apiKey: process.env.API_KEY,
+		secret: process.env.JWT_SECRET,
+		refreshSecret: process.env.JWT_REFRESH_SECRET
+	},
+	bcrypt: {
+		saltRounds: parseInt(process.env.BCRYPT_SALT)
+	},
 	logs: {
-		logLevel: process.env.LOG_LEVEL || 'error', // Configura el nivel minimo de log
-		logLevelConsole: process.env.LOG_LEVEL_CONSOLE || 'error', // Configura el nivel minimo de log en consola
-		logConsoleEnabled: process.env.LOG_CONSOLE_ENABLED || false, // Habilita o deshabilita la salida de logs en consola
-		logFileEnabled: process.env.LOG_FILE_ENABLED || false, // Habilita o deshabilita la salida de logs en archivo
+		level: process.env.LOG_LEVEL || 'error', // Configura el nivel minimo de log
+		console: {
+			level: process.env.LOG_LEVEL_CONSOLE || 'error', // Configura el nivel minimo de log en consola
+			enabled: process.env.LOG_CONSOLE_ENABLED || false // Habilita o deshabilita la salida de logs en consola
+		},
+		file: {
+			enabled: process.env.LOG_FILE_ENABLED || false // Habilita o deshabilita la salida de logs en archivo
+		}
 	}
 };
 
-module.exports = { config };
+module.exports = { requiredEnvVars, config };
