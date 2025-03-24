31fda438f5ced9bb21ff8be59dc4421f08b4e4ea
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2025-03-24 12:58:27 -0600

    refactor: Enhance token service with improved error handling and logging for token generation and verification

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -1,15 +1,55 @@
+const { logger } = require('@logger');
 const JWTManager = require('@utils/jwt.utils');
+const e = require('express');
 
+const jwtLogger = logger.injectContext('TOKEN_SERVICE');
 class TokenService {
     generateTokens(user) {
         const accessToken = JWTManager.generateAccessToken(user);
         const refreshToken = JWTManager.generateRefreshToken(user);
 
         if (!accessToken || !refreshToken) {
+            jwtLogger.error('Error generating tokens for user:', user.id);
             throw new Error('Error generating tokens');
         }
 
         return { accessToken, refreshToken };
+    }
+
+    generateAccessToken(user) {
+        try {
+            return JWTManager.generateAccessToken(user);
+        } catch (error) {
+            jwtLogger.error('Error generating access token:', error.message);
+            throw new Error('Error generating access token');
+        }
+    }
+
+    verifyAccessToken(token) {
+        try {
+            return JWTManager.verifyAccessToken(token);
+        } catch (error) {
+            jwtLogger.error('Invalid access token:', error.message);
+            throw new Error('Invalid access token');
+        }
+    }
+
+    decodeToken(token) {
+        try {
+            return JWTManager.decodeToken(token);
+        } catch (error) {
+            jwtLogger.error('Error decoding token:', error.message);
+            throw new Error('Error decoding token');
+        }
+    }
+
+    isExpired(token) {
+        try {
+            return JWTManager.isExpired(token);
+        } catch (error) {
+            jwtLogger.error('Error checking token expiration:', error.message);
+            throw new Error('Error checking token expiration');
+        }
     }
 }
 
